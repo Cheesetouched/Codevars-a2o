eee16267116f954a502d86fb91d88fd4269e8c6d
Rishabh Singh <pingrishabh@gmail.com>
2016-10-03 22:33:25 +0530

    Seventh Commit
    Used ‘RegisterUserClass’ - An External Library To Send POST Requests To
    Servers And PHP Webpages.
    
    Used External Fonts.
    
    Used Styling To Make ProgressDialog Transparent.

--- app/src/main/java/com/codevars/a2o/Login.java
+++ app/src/main/java/com/codevars/a2o/Login.java
@@ -1,29 +1,244 @@
 package com.codevars.a2o;
 
-
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.codevars.a2o.Server.RegisterUserClass;
+import com.google.android.gms.vision.text.Line;
+
+import java.util.HashMap;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class Login extends Fragment {
+public class Login extends Fragment implements View.OnClickListener {
+
+
+    private static final String LOGIN_URL = "http://atoo.esy.es/login.php";
+
+    private EditText email;
+
+    private EditText password;
+
+    private Button submit;
+
+    private LinearLayout slider;
+
+    private Animation slideup;
 
 
     public Login() {
-        // Required empty public constructor
+
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Typeface one = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Lato-Light.ttf");
+
+        View view =  inflater.inflate(R.layout.fragment_login, container, false);
+
+
+        email = (EditText) view.findViewById(R.id.email);
+
+        password = (EditText) view.findViewById(R.id.password);
+
+        submit = (Button) view.findViewById(R.id.loginbutton);
+
+        slider = (LinearLayout) view.findViewById(R.id.buttonpanel);
+
+        submit.setOnClickListener(this);
+
+        email.setTypeface(one);
+
+        password.setTypeface(one);
+
+        submit.setTypeface(one);
+
+        slide();
+
+
+        return view;
+
+
     }
+
+
+
+    private void slide() {
+
+        slideup = new TranslateAnimation(0,0,500,0);
+
+        slideup.setDuration(1000);
+
+        slider.setAnimation(slideup);
+
+    }
+
+
+
+    private boolean online() {
+
+        final ConnectivityManager internet = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return internet.getActiveNetworkInfo() != null && internet.getActiveNetworkInfo().isConnected();
+
+    }
+
+
+
+    private void check() {
+
+        if (online()) {
+
+            emptycheck();
+
+        }
+
+        else {
+
+            Toast.makeText(getContext(), "You Are Not Connected To Internet!", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+
+
+    private void emptycheck() {
+
+        if (email.getText().toString().trim().matches("")) {
+
+            Toast.makeText(getContext(), "Please Enter Your Email!", Toast.LENGTH_SHORT).show();
+
+            return;
+
+        }
+
+        if (password.getText().toString().trim().matches("")) {
+
+            Toast.makeText(getContext(), "Please Enter Your Password!", Toast.LENGTH_SHORT).show();
+
+            return;
+
+        }
+
+        else {
+
+
+            initiate();
+
+        }
+
+
+    }
+
+
+
+    private void initiate() {
+
+        String em = email.getText().toString().trim();
+
+        String pass = password.getText().toString().trim();
+
+        login(em, pass);
+
+    }
+
+
+
+    private void login(String em, String pass) {
+
+        class LoginUserClass extends AsyncTask<String, Void, String > {
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+                loading = new ProgressDialog(getContext(), R.style.LoaderTheme);
+                loading.setProgressStyle(android.R.style.Widget_ProgressBar_Small);
+                loading.setCancelable(false);
+                loading.show();
+
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+
+                if (s.equalsIgnoreCase("Successfully Logged In!")) {
+
+                    Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
+
+                    return;
+
+                }
+
+                else {
+
+                    Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
+
+                    return;
+
+                }
+
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                HashMap<String, String> credentials = new HashMap<>();
+
+                credentials.put("email", params[0]);
+
+                credentials.put("password", params[1]);
+
+                RegisterUserClass ruc = new RegisterUserClass();
+
+                String result = ruc.sendPostRequest(LOGIN_URL, credentials);
+
+                return result;
+
+            }
+
+        }
+
+        LoginUserClass attempt = new LoginUserClass();
+
+        attempt.execute(em, pass);
+
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == submit) {
+
+            check();
+
+        }
+
+    }
+
+
 
 }
